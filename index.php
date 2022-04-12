9682408c684759d80c0257c309f114a9d1c5cec0
MGPasinetti <mg.pasinetti@gmail.com>
2022-04-12 15:43:04 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!-- 
+Stampare a schermo una decina di dischi musicali (vedi screenshot).
+Utilizzare:
+Html, CSS, VueJS, axios, PHP
+NOTA SU COME STRUTTURARE LA REPO: per la seconda mileston rifate il frontend usando Vue ed Axios da CDN (come visto a lezione) e non da CLI. Mettete i file html e js in una sottocartella.
+Prima Milestone:
+Stampiamo i dischi solo con l’utilizzo di PHP, che stampa direttamente i dischi in pagina: al caricamento della pagina ci saranno tutti i dischi.
+Seconda Milestone:
+Attraverso l’utilizzo di axios: al caricamento della pagina axios chiederà, attraverso una chiamata api, i dischi a php e li stamperà attraverso vue.
+Bonus:
+Attraverso un’altra chiamata api, filtrare gli album per genere
+vue-dischi.png  
+-->
