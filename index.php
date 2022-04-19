c2607b4b7d29ed022312de8f076fb53b29dc1829
MGPasinetti <mg.pasinetti@gmail.com>
2022-04-19 09:10:31 +0200

    style fixing

--- index.php
+++ index.php
@@ -9,7 +9,6 @@ Seconda Milestone:
 Attraverso l’utilizzo di axios: al caricamento della pagina axios chiederà, attraverso una chiamata api, i dischi a php e li stamperà attraverso vue.
 Bonus:
 Attraverso un’altra chiamata api, filtrare gli album per genere
-vue-dischi.png
 -->
 
 <?php
